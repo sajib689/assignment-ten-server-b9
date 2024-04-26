b9427622507284350345f015fbc19c0a04669f15
sajib689 <jibonraja11@gmail.com>
2024-04-26 13:13:21 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.2m0rny5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', async (req, res) => {
